e9d244009e2241b91c77af1ab550330001ad38ff
neslihan özdemir <33529909+neslihan67@users.noreply.github.com>
2021-05-30 23:37:23 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,168 +1,226 @@
-<?php
 
-//MUHTAR Kurumsal İndexxxxx KURUMSAL
+<?php
+//dısarıdan kımselerin hataları gormesini engeller.
+error_reporting(1);
+ini_set("display_errors", 1);
+
 
 @session_start();
 @ob_start();
 
 define("DATA","data/");
 define("SAYFA","include/");
-define("SINIF","admin/sinif/");
+define("SINIF","sinif/");
 
 include_once(DATA."connection.php");
-define("SITE",$siteUrl);
-
-include_once(SINIF."class.phpmailer.php");
-include_once(SINIF."class.smtp.php");
-
-?>
+define("SITE",$siteUrl."admin/");
 
 
-<!doctype html>
-<html class="no-js" lang="zxx">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-	
-	 <title><?=$siteTarayiciAd?></title>
-  <meta name="description" content="<?=$siteAciklama?>">
-  <meta name="keywords" content="<?=$siteAnahtar?>">
-  <meta name="author" content="Neslihan Özdemir">
-  
- 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- <link rel="manifest" href="site.webmanifest"> -->
-	 <?php
-	$veri=$DB->VeriGetir("logo","","","ORDER  BY ID ASC",1);
-	
-	?>
-	<link rel="shortcut icon" type="image/x-icon" href="<?=SITE?>admin/images/SiteLogo/<?=$veri[0]["Resim"]?>">
-    
-    <!-- Place favicon.ico in the root directory -->
-	
-  <!--	ATOMATİK SLAYT KUTUPHANESİ-->
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <!-- CSS here -->
-    <link rel="stylesheet" href="<?=SITE?>css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=SITE?>css/owl.carousel.min.css">
-    <link rel="stylesheet" href="<?=SITE?>css/magnific-popup.css">
-    <link rel="stylesheet" href="<?=SITE?>css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?=SITE?>css/themify-icons.css">
-    <link rel="stylesheet" href="<?=SITE?>css/nice-select.css">
-    <link rel="stylesheet" href="<?=SITE?>css/flaticon.css">
-    <link rel="stylesheet" href="<?=SITE?>css/gijgo.css">
-    <link rel="stylesheet" href="<?=SITE?>css/animate.css">
-    <link rel="stylesheet" href="<?=SITE?>css/slick.css">
-    <link rel="stylesheet" href="<?=SITE?>css/slicknav.css">
-
-	
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
-
-    <link rel="stylesheet" href="<?=SITE?>css/style.css">
-    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
-</head>
-
-<body>
-  
-
-<?php
-
- include_once(DATA."ust.php");
- 
- 
-	if($_GET && !empty($_GET["sayfa"]))
+ if(!empty($_SESSION["ID"]) && (!empty($_SESSION["adsoyad"])) && (!empty($_SESSION["telefon"])))
 	{
-		$sayfa=$_GET["sayfa"].".php";
-			if(file_exists(SAYFA.$sayfa))
-			{
-				include_once(SAYFA.$sayfa);
-			}
-			else
-			{
-				include_once(SAYFA."home.php");// include/home.php
-			}
-
+			
+		
 	}
 	else
 	{
- 
-		include_once(SAYFA."home.php");
+		?>
+		<meta http-equiv="refresh" content="1;url=<?=SITE?>giris-yap">
+        <?php
+		exit();
 	}
-	
-	include_once(DATA."footer.php");
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?=$siteTarayiciAd?></title>
+  <meta name="description" content="<?=$siteAciklama?>">
+  <meta name="keywords" content="<?=$siteAnahtar?>">
+  <meta name="author" content="Neslihan Özdemir">
+  <?php
+	$veri=$DB->VeriGetir("logo","","","ORDER  BY ID ASC",1);
 	
 	?>
 	
+    <link rel="shortcut icon" type="image/x-icon" href="<?=SITE?>images/SiteLogo/<?=$veri[0]["Resim"]?>">
 
-   
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/fontawesome-free/css/all.min.css">
+  
+  <!--SUMMER NOTE M E   -->
+
+    <link rel="stylesheet" href="<?=SITE?>plugins/summernote/summernote-bs4.css">
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/jqvmap/jqvmap.min.css">
+<!-- CodeMirror -->
+<script src="<?=SITE?>plugins/codemirror/codemirror.js"></script>
+<script src="<?=SITE?>plugins/codemirror/mode/css/css.js"></script>
+<script src="<?=SITE?>plugins/codemirror/mode/xml/xml.js"></script>
+<script src="<?=SITE?>plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?=SITE?>dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/summernote/summernote-bs4.css">
+  
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?=SITE?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?=SITE?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?=SITE?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  
 
 
 
-    <!-- link that opens popup -->
-    <script src=" https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"> </script>
-    <!-- JS here -->
-    <script src="<?=SITE?>js/vendor/modernizr-3.5.0.min.js"></script>
-    <script src="<?=SITE?>js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="<?=SITE?>js/popper.min.js"></script>
-    <script src="<?=SITE?>js/bootstrap.min.js"></script>
-    <script src="<?=SITE?>js/owl.carousel.min.js"></script>
-    <script src="<?=SITE?>js/isotope.pkgd.min.js"></script>
-    <script src="<?=SITE?>js/ajax-form.js"></script>
-    <script src="<?=SITE?>js/waypoints.min.js"></script>
-    <script src="<?=SITE?>js/jquery.counterup.min.js"></script>
-    <script src="<?=SITE?>js/imagesloaded.pkgd.min.js"></script>
-    <script src="<?=SITE?>js/scrollIt.js"></script>
-    <script src="<?=SITE?>js/jquery.scrollUp.min.js"></script>
-    <script src="<?=SITE?>js/wow.min.js"></script>
-    <script src="<?=SITE?>js/nice-select.min.js"></script>
-    <script src="<?=SITE?>js/jquery.slicknav.min.js"></script>
-    <script src="<?=SITE?>js/jquery.magnific-popup.min.js"></script>
-    <script src="<?=SITE?>js/plugins.js"></script>
-    <!-- <script src="js/gijgo.min.js"></script> -->
-    <script src="<?=SITE?>js/slick.min.js"></script>
-
-	
-	
-
-
-
-    <!--contact js-->
-    <script src="<?=SITE?>js/contact.js"></script>
-    <script src="<?=SITE?>js/jquery.ajaxchimp.min.js"></script>
-    <script src="<?=SITE?>js/jquery.form.js"></script>
-    <script src="<?=SITE?>js/jquery.validate.min.js"></script>
-    <script src="<?=SITE?>js/mail-script.js"></script>
-	<script src="<?=SITE?>js/main.js"></script>
-
-
-   
-	<script>
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlidesOne");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+ <?php 
+  include_once(DATA."ustmenu.php");//  data/ustmenu.php
+  include_once(DATA."menu.php"); //data/menu.php
+  
+  if($_GET && !empty($_GET["sayfa"]))
+{
+  $sayfa=$_GET["sayfa"].".php";
+  if(file_exists(SAYFA.$sayfa))
+  {
+    include_once(SAYFA.$sayfa);
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 2 seconds
-}
-
-
-</script>
-<!---->
-
-
-							<script>
-
+  else{
+    include_once(SAYFA."home.php");// include/home.php
 	
-</body>
+  }
 
+}
+else
+{
+ 
+  include_once(SAYFA."home.php");
+}
+  
+  include_once(DATA."footer.php"); // data/footer.php
+  
+  ?>
+
+
+
+
+
+
+
+  
+  
+
+
+
+</div>
+
+
+<!-- jQuery -->
+<script src="<?=SITE?>plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?=SITE?>plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?=SITE?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- ChartJS -->
+<script src="<?=SITE?>plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="<?=SITE?>plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="<?=SITE?>plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?=SITE?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?=SITE?>plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?=SITE?>plugins/moment/moment.min.js"></script>
+<script src="<?=SITE?>plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?=SITE?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="<?=SITE?>plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?=SITE?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=SITE?>dist/js/adminlte.js"></script>
+<!-- jQuery -->
+<script src="<?=SITE?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?=SITE?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=SITE?>dist/js/adminlte.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="<?=SITE?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="<?=SITE?>plugins/jszip/jszip.min.js"></script>
+<script src="<?=SITE?>plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?=SITE?>plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?=SITE?>plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?=SITE?>plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+
+ 
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?=SITE?>dist/js/pages/dashboard.js"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+<!-- Summernote -->
+<script src="<?=SITE?>plugins/summernote/summernote-bs4.min.js"></script>
+<script>
+  $(function () {
+    // Summernote
+    $('.textarea').summernote(),
+	filebrowserUploadUrl:"index.php"
+  })
+</script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="<?=SITE?>dist/js/demo.js"></script>
+</body>
 </html>
